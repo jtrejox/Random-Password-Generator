d2109a143230647bcd9ab8a02e8f87b90a36d6e8
Jorge Trejo <jtrejo@mail.com>
2020-10-29 22:03:04 -0700

    added comments to the .js file

--- Develop/script.js
+++ Develop/script.js
@@ -17,23 +17,26 @@ var charCompare = ""
 
 // Write password to the #password input
 
-
 function writePassword() {
+//PROMPT TO INPUT PASSWORD LENGTH==============================================================================
   passwordLength = prompt("how long would you like your Password to be?(Must be between 8 and 128 characters")
     if (passwordLength >= 7 && passwordLength <= 128){
       alert("You've chosen a " + passwordLength + " character long password");
     }
+//ERROR MESSAGE IF AN INVALID INPUT IS ENTERED=================================================================
     else{
       alert("Invalid Input");
       writePassword()
     }
   var arrcount = 0
+//START OF "WHILE LOOP" TO SELECT WHICH ARRAYS WILL BE USED TO GENERATE THE PASSWORD===========================
   while(arrcount === 0){
     
     arrcount = 4;
+//MAIN ARRAY OF CHARACTER ARRAYS===============================================================================
     var passwordArr = [upperArr, lowerArr, numbersArr, specialArr]
+//"FOR LOOP" TO SELECT WHICH ARRAYS WILL BE USED TO GENERATE THE PASSWORD======================================
     for (var i = 0, len = charNames.length; i < len; i++){
-      
       var charSelection = charNames[i];
       var arrChoice = confirm("Would you like to use " + charSelection + " ?");
       if (arrChoice) {
@@ -50,15 +53,19 @@ function writePassword() {
         alert("You must chose at least 1 set of characters");
       }
     }
-  }  
+  }
+  
+//SECCTION WHERE "generatePassword" FUNCTION IS CALLED WITH THE APROPRIATE VARIABLES BEING PASSED AND WHERE IT IS WRITEN TO THE DOM== 
   generatePassword(passwordLength, passwordArr)
   var passwordText = document.querySelector("#password");
   passwordText.value = pass;   
    } 
-  
+
+//RANDOM NUMBER FUNCTION USED IN "generatePassword" FUNCTION=======================================================================
 function randomNum(x){return Math.floor(Math.random() * x)}
 
 //generatePassword() with placeholders
+//"generatePassword" FUNCTION, WHERE THE PASSWORD IS ASSEMBLED AND CREATED
 function generatePassword(passwordLength, passwordArr){
   var pswrdArr = []
   for (var i = 0; i < passwordLength;i++ ){
@@ -70,5 +77,5 @@ function generatePassword(passwordLength, passwordArr){
      pass = pswrdArr.join("");
   }
  }
-
+//EVENT LISTENER FOR THE BUTTON THAT GENERATES THE PASSWORD IN THE DOM
 generateBtn.addEventListener("click", writePassword);
